3d52f3b2665e2a7125d81254ec5a5fed6aba7c95
Linux <wenqiang1208@126.com>
2017-07-19 23:22:10 -0700

    测试数据序列化

--- client/chat_client.cpp
+++ client/chat_client.cpp
@@ -1,7 +1,7 @@
 
 
 #include "udp_client.h"
-
+#include "data.h"
 void usage(const char* arg)
 {
 	std::cout<<"Usage: "<<arg<<"[client_ip] [client_port]" <<std::endl;
@@ -17,19 +17,34 @@ int main(int argc,char*argv[])
 	client.init_client();
 	while(1)
 	{
+		data w;
+		std::cout<<"please enter nick_name:";
+		std::cin>>w.nick_name;
+		std::cout<<"please enter school:";
+		std::cin>> w.school;
 		std::cout<<"Please Enter# ";
-		fflush(stdout);
-		char buf[1024];
-		ssize_t s = read(0,buf,sizeof(buf)-1);
-		if(s >0)
-		{
-			buf[s] = 0;
-			int ret = client.send_msg(buf);
-		}
+		std::cin >> w.msg;
+		//fflush(stdout);
+		//char buf[1024];
+		//ssize_t s = read(0,buf,sizeof(buf)-1);
+		//if(s >0)
+		//{
+		//	buf[s] = 0;
+		//	
+		//	int ret = client.send_msg(buf);
+		//}
 		std::string msg;
-		client.recv_msg(msg);
+		w.data_to_string(msg);
+		client.send_msg(msg);
+		std::string out;
+		client.recv_msg(out);
 		std::cout <<"server echo# "<<msg<<std::endl;
+		data r;
+		r.string_to_data(out);
+		std::cout<<r.nick_name<<"-"<<r.school<<"-"<<r.msg<<std::endl;
+		
 	}
+
 	return 0;
 }
 
